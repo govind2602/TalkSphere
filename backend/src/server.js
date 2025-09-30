970ee700ebeaffad584630c6b528f9b35286ee4d
govind2602 <govindchaubey638@gmail.com>
2025-09-30 18:30:15 +0530

    Describe your changes here

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,10 +26,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chats", chatRoutes);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 app.listen(PORT, () => {
